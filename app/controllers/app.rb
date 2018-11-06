4d9a8ff0f255cc90c5b13635b9226f0dedf2fafd
VicXu <xumingyo@gmail.com>
2018-11-06 16:59:01 +0800

    fix gallery

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -23,7 +23,50 @@ module IssPay
           routing.multi_route
 
           routing.get do
-            { "message" => "ISS Pay Api is running." }
+            {
+              "messages": [
+                 {
+                   "attachment":{
+                     "type":"template",
+                     "payload":{
+                       "template_type":"generic",
+                       "image_aspect_ratio": "square",
+                       "elements":[
+                         {
+                           "title":"Chatfuel Rockets Jersey",
+                           "image_url":"https://rockets.chatfuel.com/assets/shirt.jpg",
+                           "subtitle":"Size: M",
+                           "buttons":[
+                             {
+                               "type":"web_url",
+                               "url":"https://rockets.chatfuel.com/store",
+                               "title":"View Item"
+                             }
+                           ]
+                         },
+                         {
+                           "title":"Chatfuel Rockets Jersey",
+                           "image_url":"https://rockets.chatfuel.com/assets/shirt.jpg",
+                           "subtitle":"Size: L",
+                           "default_action": {
+                             "type": "web_url",
+                             "url": "https://rockets.chatfuel.com/store",
+                             "messenger_extensions": true
+                           },
+                           "buttons":[
+                             {
+                               "type":"web_url",
+                               "url":"https://rockets.chatfuel.com/store",
+                               "title":"View Item"
+                             }
+                           ]
+                         }
+                       ]
+                     }
+                   }
+                 }
+               ]
+             }
           end
         end
       end
