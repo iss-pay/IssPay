3b044324565ef167127e94f701b4c252051e8f85
VicXu <xumingyo@gmail.com>
2018-11-06 19:46:27 +0800

    api msg

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -30,6 +30,54 @@ module IssPay
              }
           end
         end
+        routing.on 'test' do
+          routing.get do
+            {
+              "messages": [
+                 {
+                   "attachment":{
+                     "type":"template",
+                     "payload":{
+                       "template_type":"list",
+                       "top_element_style":"large",
+                       "elements":[
+                         {
+                           "title":"Chatfuel Rockets Jersey",
+                           "image_url":"http://rockets.chatfuel.com/assets/shirt.jpg",
+                           "subtitle":"Size: M",
+                           "buttons":[
+                             {
+                               "type":"web_url",
+                               "url":"https://rockets.chatfuel.com/store",
+                               "title":"View Item"
+                             }
+                           ]
+                         },
+                         {
+                           "title":"Chatfuel Rockets Jersey",
+                           "image_url":"http://rockets.chatfuel.com/assets/shirt.jpg",
+                           "subtitle":"Size: L",
+                           "default_action": {
+                             "type": "web_url",
+                             "url": "https://rockets.chatfuel.com/store",
+                             "messenger_extensions": true
+                           },
+                           "buttons":[
+                             {
+                               "type":"web_url",
+                               "url":"https://rockets.chatfuel.com/store",
+                               "title":"View Item"
+                             }
+                           ]
+                         }
+                       ]
+                     }
+                   }
+                 }
+               ]
+             }
+          end
+        end
       end
 
       routing.root do
