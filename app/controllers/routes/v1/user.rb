d3509b830fd52920bfb203fa3366b39fe050f38e
VicXu <xumingyo@gmail.com>
2018-11-10 15:55:04 +0800

    register msg response

--- app/controllers/routes/v1/user.rb
+++ app/controllers/routes/v1/user.rb
@@ -14,6 +14,19 @@ module IssPay
 
       routing.post do
         puts routing.params
+        data = {
+          'message_id' => routing.params['messenger user id'],
+          'last_name' => routing.params['last name'],
+          'first_name' => routing.params['first name'],
+          'member_type' => routing.params['MemberType']
+        }
+        user = User.find(message_id: data['message_id'])
+        if !user.nil?
+          Representer::ChatBotMsg.new("Hello #{user.first_name}，你已經註冊過了喔~~")
+        else
+          user = User.create(data)
+          Representer::ChatBotMsg.new("Hello #{user.first_name}，成功註冊為#{user.member_type}!!")
+        end
       end
     end
   end
