a5aeead7598dcee3e673644c6820728ba925d49a
VicXu <xumingyo@gmail.com>
2018-11-10 18:16:56 +0800

    purchase item

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -18,6 +18,16 @@ module IssPay
       self.created_at ||= Time.now
     end
 
+    def after_create
+      
+      if type == 'purchase'
+        purchased_item = Item.find(id: item_id)
+        quantity = purchased_item.quantity
+        purchased_item.quantity = quantity -1  
+        purchased_item.save
+      end
+    end
+
     def paid?
       self.status == 1
     end
