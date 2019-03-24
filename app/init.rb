20523f0b6a4eb031fb5d1c6f274df749d28a40e7
vicxu <xumingyo@gmail.com>
2019-03-24 17:00:42 +0800

    fix

--- app/init.rb
+++ app/init.rb
@@ -1,4 +1,4 @@
-folders = %w[models values representers services controllers policies]
+folders = %w[models values representers services controllers]
 
 folders.each do |folder|
   require_relative "./#{folder}/init.rb"
